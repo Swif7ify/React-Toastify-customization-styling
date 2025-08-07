6df723c984839d7de63cf3eed598917fb345ae38
Rin <165538982+Swif7ify@users.noreply.github.com>
2025-08-07 12:04:48 +0800

    Create toast.tsx

---
+++ toast.tsx
@@ -0,0 +1,82 @@
+import { toast } from "react-toastify";
+import React from "react";
+import { Slide } from "react-toastify";
+
+export function toastSuccess(message: string, description?: string) {
+	toast.success(
+		<div style={{ display: "flex" }}>
+			<span className="mr-2" />
+			<div>
+				<strong>{message}</strong>
+				{description && <div style={{ fontSize: "0.9em", marginTop: "4px" }}>{description}</div>}
+			</div>
+		</div>,
+		{
+			position: "bottom-right",
+			autoClose: 999999,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			theme: "colored",
+			transition: Slide,
+		}
+	);
+}
+
+export function toastError(message: string, description?: string) {
+	toast.error(
+		<>
+			<strong>{message}</strong>
+			{description && <div style={{ fontSize: "0.9em" }}>{description}</div>}
+		</>,
+		{
+			position: "bottom-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			theme: "colored",
+			transition: Slide,
+		}
+	);
+}
+
+export function toastInfo(message: string, description?: string) {
+	toast.info(
+		<>
+			<strong>{message}</strong>
+			{description && <div style={{ fontSize: "0.9em" }}>{description}</div>}
+		</>,
+		{
+			position: "bottom-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			theme: "colored",
+			transition: Slide,
+		}
+	);
+}
+
+export function toastWarning(message: string, description?: string) {
+	toast.warn(
+		<>
+			<strong>{message}</strong>
+			{description && <div style={{ fontSize: "0.9em" }}>{description}</div>}
+		</>,
+		{
+			position: "bottom-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			theme: "colored",
+			transition: Slide,
+		}
+	);
+}
